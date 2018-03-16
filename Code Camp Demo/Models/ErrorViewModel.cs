15cb0b4b9bed40cde1bf2d846629cc78b73929ae
Garrett Curtis <Garrett@Cyber-Medics.com>
2018-03-16 18:41:50 -0400

    Add project files.

---
+++ Code Camp Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Code_Camp_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
