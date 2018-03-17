65adf026d905d8c7ed76f1822e3a3b2bbea69691
Garrett Curtis <Garrett@Cyber-Medics.com>
2018-03-16 22:31:58 -0400

    Added Application Insights

--- Code_Camp_Demo/Program.cs
+++ Code_Camp_Demo/Program.cs
@@ -19,6 +19,7 @@ namespace Code_Camp_Demo
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
