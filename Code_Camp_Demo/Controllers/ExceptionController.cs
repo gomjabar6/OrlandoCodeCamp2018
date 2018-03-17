4d368ae21433d6d61145807dcbf306fb8e9caccd
Garrett Curtis <Garrett@Cyber-Medics.com>
2018-03-16 23:31:11 -0400

    Added fluff for demo

---
+++ Code_Camp_Demo/Controllers/ExceptionController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Code_Camp_Demo.Controllers
+{
+    public class ExceptionController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            SuperCoolFunction();
+
+           
+            return View();
+        }
+
+        private void SuperCoolFunction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Code_Camp_Demo/Controllers/HomeController.cs
+++ Code_Camp_Demo/Controllers/HomeController.cs
@@ -5,11 +5,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Code_Camp_Demo.Models;
+using System.Threading;
+using Microsoft.ApplicationInsights;
 
 namespace Code_Camp_Demo.Controllers
 {
     public class HomeController : Controller
     {
+        private TelemetryClient telemetry = new TelemetryClient();
+
         public IActionResult Index()
         {
             return View();
@@ -17,14 +21,19 @@ namespace Code_Camp_Demo.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Welcome to Orlando Code Camp 2018!";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            // Add some delay for this one
+            Random rnd = new Random();
+            Thread.Sleep(rnd.Next(1, 4) * 1000);
+            
+
+            ViewData["Message"] = "Created by CyberMedics, LLC.";
 
             return View();
         }
@@ -32,6 +41,12 @@ namespace Code_Camp_Demo.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult OpenSlides()
+        {
+            telemetry.TrackEvent("OpenSlides");
+            return Redirect(string.Format("https://cybermedics-my.sharepoint.com/:p:/g/personal/garrett_gscurtis_com/ESgK65gUkN9Mt70tM8sk_BoBcxxXx7rugCIZXDp3UMWazQ?e=PWN86w"));
         }
     }
 }
